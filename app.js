4927d521879d19d4ac4612889ad20a62514c3a82
akshaya kini <akshaya.kini12@gmail.com>
2023-11-22 13:37:49 +0100

    Create functionality to add items to respective columns

--- app.js
+++ app.js
@@ -8,3 +8,51 @@ const backlogListEl = document.getElementById("backlog-list");
 const progressListEl = document.getElementById("progress-list");
 const completeListEl = document.getElementById("complete-list");
 const onHoldListEl = document.getElementById("on-hold-list");
+
+let backlogListArray = [];
+let progressListArray = [];
+let completeListArray = [];
+let onHoldListArray = [];
+let listArrays = [
+  backlogListArray,
+  progressListArray,
+  completeListArray,
+  onHoldListArray,
+];
+
+// create list items for given column
+function createElement(index, text) {
+  const listItem = document.createElement("li");
+  listItem.classList.add("drag-item");
+  listItem.textContent = text;
+  listColumns[index].appendChild(listItem);
+}
+
+// add created item to list array
+function addToList(index) {
+  const text = addItems[index].textContent;
+
+  listArrays[index].push(text);
+  createElement(index, text);
+}
+
+// show input box for adding item
+function showInputBox(index) {
+  addBtns[index].style.visibility = "hidden";
+  saveItemBtns[index].style.display = "flex";
+  addItemContainers[index].style.display = "flex";
+}
+
+// hide input box on saving item
+function hideInputBox(index) {
+  addBtns[index].style.visibility = "visible";
+  saveItemBtns[index].style.display = "none";
+  addItemContainers[index].style.display = "none";
+  addToList(index);
+  addItems[index].textContent = "";
+}
+
+// event listener for add items
+addBtns.forEach((btn, index) =>
+  btn.addEventListener("click", () => showInputBox(index))
+);
